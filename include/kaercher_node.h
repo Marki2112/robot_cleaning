17d0e46a0c60eac013adb13703e30150c9dcde6c
Markus Joos <joos_markus@t-online.de>
2023-06-18 04:38:36 -0700

    adding the class in the header-file

--- include/kaercher_node.h
+++ include/kaercher_node.h
@@ -1,3 +1,49 @@
 #ifndef KAERCHER_H_
 #define KAERCHER_H_
+
+#include <jsoncpp/json/value.h>
+
+#include "ros/ros.h"
+#include <ros/console.h>
+
+
+class cleaning_robot{
+
+    public:
+        cleaning_robot();
+        ~cleaning_robot();
+
+        Json::Value robot_info;
+        double calculatePath(Json::Value robot_path);
+        double calculateCleaningArea(double lengthCleaningGadget, double path);
+        double calculateTimeOfPath();
+        Json::Value getRobotInfo(std::string info) const;
+
+    private:
+        std::string jsonfile;
+        Json::Value readJsonFIle(std::string jsonfile);
+        double calculateDistance(double x1, double y1, double x2, double y2);
+        double calculateVelocity(float Kappa);
+};
+
+cleaning_robot::cleaning_robot(){
+    ros::NodeHandle private_nh("~");
+    
+    // Check if json available?
+    if(private_nh.getParam("jsonfile", jsonfile)){
+        ROS_INFO("Read Json-File");
+        robot_info = readJsonFIle(jsonfile);
+    }
+    else{
+        // aus Programm springen
+        ROS_ERROR("JSON-File is not available"); 
+    }
+
+}
+
+cleaning_robot::~cleaning_robot(){  
+
+}
+
+
 #endif
