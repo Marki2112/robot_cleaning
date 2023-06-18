189e05bdea4a8200b2a3d6a0f160d2c9a4156054
Markus Joos <joos_markus@t-online.de>
2023-06-18 04:39:09 -0700

    calculating the robot_path from the parameter in the json-file

--- src/kaercher_node.cpp
+++ src/kaercher_node.cpp
@@ -1,70 +1,39 @@
 #include <iostream>
 #include <fstream>
-#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/reader.h>
 #include <string>
 #include <cmath>
-#include <vector>
 
 #include "ros/ros.h"
 #include <ros/console.h>
 
-class cleaning_robot{
+#include "kaercher_node.h"
 
-    public:
-        cleaning_robot();
-        ~cleaning_robot();
-
-
-    private:
-        std::string jsonfile;
-        Json::Value robot_info;
-        Json::Value readJsonFIle(std::string jsonfile);
-        double calculateDistance(double x1, double y1, double x2, double y2);
-        double calculatePath(std::vector<double>& x_points, std::vector<double>& y_points);
-        double calculateVelocity(float Kappa);
-        double calculateCleaningArea(double lengthCleaningGadget, double path);
-        double calculateTimeOfPath();
-};
-
-cleaning_robot::cleaning_robot(){
-    ros::NodeHandle private_nh("~");
-    
-    // Check if json available?
-    if(private_nh.getParam("jsonfile", jsonfile)){
-        ROS_INFO("Read Json-File");
-        robot_info = readJsonFIle(jsonfile);
-    }
-    else{
-        // aus Programm springen
-        ROS_ERROR("JSON-File is not available"); 
-    }
-
-}
-
-cleaning_robot::~cleaning_robot(){
-    
-}
 
 Json::Value cleaning_robot::readJsonFIle(std::string jsonfile){
     std::ifstream robot_file(jsonfile);
     Json::Value robot_information;
     Json::Reader reader;
     reader.parse(robot_file, robot_information);
+    //checking if the json file has robot, cleaning gadget and path --> if not throw an fail
     return robot_information;
 }
 
 double cleaning_robot::calculateDistance(double x1, double y1, double x2, double y2){
     return std::sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 } 
-// Changing Datetype to make it more flexible
-double cleaning_robot::calculatePath(std::vector<double>& x_points, std::vector<double>& y_points){
+
+double cleaning_robot::calculatePath(Json::Value robot_path){
+    
     double total_distance = 0.0;
 
-    for(size_t i = 0; i < x_points.size() - 1; i++){
-        double x1 = x_points[i];
-        double y1 = y_points[i];
-        double x2 = x_points[i + 1];
-        double y2 = y_points[i + 1];
+    // Calculating not the exact path 
+    for(Json::ArrayIndex i = 0; i < robot_path.size() - 1; i++){
+        double x1 = robot_path[i][0].asDouble();
+        double y1 = robot_path[i][1].asDouble();
+        double x2 = robot_path[i][0].asDouble();
+        double y2 = robot_path[i][1].asDouble();
+    
 
         total_distance = calculateDistance(x1, y1, x2, y2);
         total_distance += total_distance;
@@ -121,9 +90,22 @@ double cleaning_robot::calculateTimeOfPath(){
     return 0.0;
 }
 
+Json::Value cleaning_robot::getRobotInfo(std::string info) const {
+    return robot_info[info];
+}
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "kaercher");
     cleaning_robot cleaning_robot;
+
+    // getting path information from json-file
+    Json::Value robot_path = cleaning_robot.getRobotInfo("path");
+    // calculating the total_distance and printing out
+    double total_distance = cleaning_robot.calculatePath(robot_path);
+    std::cout << "The total_distance is: " << total_distance << std::endl;
+
+    
+
 
     ros::spin();
 }
