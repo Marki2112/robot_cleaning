82e1b416c3869590ca95237a6321b1772faa3d6f
Markus Joos <joos_markus@t-online.de>
2023-06-18 08:35:37 -0700

    adding the function to calculate the velocity and the time depends on the curvature of the path

--- src/kaercher_node.cpp
+++ src/kaercher_node.cpp
@@ -1,14 +1,20 @@
+/*
+name: Markus Joos
+Date: 18.06.2023
+*/
+
 #include <iostream>
 #include <fstream>
 #include <jsoncpp/json/reader.h>
 #include <string>
 #include <cmath>
+#include <vector>
+#include <numeric>
 
 #include "ros/ros.h"
 #include <ros/console.h>
 
 #include "kaercher_node.h"
-
 
 Json::Value cleaning_robot::readJsonFIle(std::string jsonfile){
     std::ifstream robot_file(jsonfile);
@@ -19,32 +25,56 @@ Json::Value cleaning_robot::readJsonFIle(std::string jsonfile){
     return robot_information;
 }
 
-double cleaning_robot::calculateDistance(double x1, double y1, double x2, double y2){
-    return std::sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-} 
 
-double cleaning_robot::calculatePath(Json::Value robot_path){
+std::vector<double> cleaning_robot::calculateDistance(Json::Value robot_path){
     
-    double total_distance = 0.0;
+    std::vector<double> distance_vec;
+    std::pair<std::vector<double>, std::vector<double>> points = createVector(robot_path);
+    
+    std::vector<double> x_points = points.first;
+    std::vector<double> y_points = points.second;
 
     // Calculating not the exact path 
-    for(Json::ArrayIndex i = 0; i < robot_path.size() - 1; i++){
-        double x1 = robot_path[i][0].asDouble();
-        double y1 = robot_path[i][1].asDouble();
-        double x2 = robot_path[i+1][0].asDouble();
-        double y2 = robot_path[i+1][1].asDouble();
+    for(size_t i = 0; i < x_points.size() - 1; i++){
+        double x1 = x_points[i];
+        double y1 = y_points[i];
+        double x2 = x_points[i+1];
+        double y2 = y_points[i+1];
 
-        double distance = calculateDistance(x1, y1, x2, y2);
+        double dist = std::sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        distance_vec.push_back(dist);
+    }
 
-        total_distance += distance;
- 
+    return distance_vec;
+}
+
+// Creating from json_file an vector;
+std::pair<std::vector<double>, std::vector<double>> cleaning_robot::createVector(Json::Value robot_path){
+    
+    std::vector<double> x_points, y_points;
+    
+    for(Json::ArrayIndex i = 0; i < robot_path.size(); i++){
+        double x = robot_path[i][0].asDouble();  
+        double y = robot_path[i][1].asDouble();
+        x_points.push_back(x);
+        y_points.push_back(y);
+    }
+
+    return std::make_pair(x_points, y_points);
+}
+
+double cleaning_robot::calculatePath(std::vector<double> distance){
+    double total_distance = 0.0;
+
+    for(size_t i = 0; i < distance.size(); i++){
+        total_distance += distance[i];
     }
     return total_distance;
 }
 
-double cleaning_robot::calculateVelocity(float kappa){
+double cleaning_robot::calculateVelocity(double curvature){
     /*
-    Kappa is be calculated from the path --> Curvature
+    Kappa is be calculated from the path --> Curvature kappa 1/distance approximately
     possile three velocity outcomes:
     1. straights and flat curves            --> max Velocity
     2. kappa is given                       --> calculate the velocity
@@ -52,22 +82,28 @@ double cleaning_robot::calculateVelocity(float kappa){
     */  
 
     double velRobot;
-    float kappa_krit = 0.5;                 // Unit 1/m
-    float kappa_max = 10;                   // Unit 1/m
-    float vmin = 0.15;                      // Unit m/s
-    float vmax = 1.1;                       // Unit m/s
-    float v_kappa;                          // Unit m/s
+    double k_krit = 0.5;                        // Unit 1/m
+    double k_max = 10.0;                        // Unit 1/m
+    double vmin = 0.15;                         // Unit m/s
+    double vmax = 1.1;                          // Unit m/s
 
-    // calculate Velocity of the robot, if kappa is given
-    v_kappa = vmax - ((vmax-vmin)/(kappa_max - kappa_krit))*(kappa-kappa_krit);
-    
     // Check if kappa bigger then kappa_max 
-    if(kappa > kappa_max){
+    bool test1 = curvature <= k_krit;
+    bool test2 = 0.5 <= curvature && curvature < 10.0;
+    bool test3 = k_max <= curvature;
+
+    if(curvature <= k_krit){
+        velRobot = vmax;
+    }
+    else if(k_max <= curvature){
         velRobot = vmin;
     }
-    else{
-        velRobot = v_kappa;
+    else if(k_krit <= curvature && curvature < k_max){
+        velRobot = vmax - ((vmax-vmin)/(k_max - k_krit))*(curvature-k_krit);
     }
+
+    std::cout << curvature << " " << velRobot << " " << test1 << " " << test3 << " " << test2 << std::endl;
+
     return velRobot;
 }
 
@@ -76,6 +112,8 @@ double cleaning_robot::calculateCleaningArea(Json::Value robot, Json::Value leng
     Need the lenghts of the cleaning Gadget from the Json
     Need the complete Path of the Robot
     */
+
+
 
     double cleaningArea=0.0;
 
@@ -86,8 +124,19 @@ double cleaning_robot::calculateCleaningArea(Json::Value robot, Json::Value leng
 }
 
 // Need Velocity and Path to get the time
-double cleaning_robot::calculateTimeOfPath(){
-    return 0.0;
+double cleaning_robot::calculateTimeOfPath(std::vector<double> distance){
+    double curvature, total_time, time, velocity;
+
+    for(size_t i = 0; i < distance.size(); i++){
+        double dist = distance[i];
+        curvature = 1/dist;
+        velocity = calculateVelocity(curvature);
+        //std::cout << velocity << " " << dist << " " << curvature << std::endl;
+        time = dist/velocity;
+        total_time += time;
+    }
+
+    return total_time;
 }
 
 Json::Value cleaning_robot::getRobotInfo(std::string info) const {
@@ -105,12 +154,17 @@ int main(int argc, char** argv){
     Json::Value robot = cleaning_robot.getRobotInfo("robot");
 
     // calculating the total_distance and printing out
-    double total_distance = cleaning_robot.calculatePath(robot_path);
+    std::vector<double> distance = cleaning_robot.calculateDistance(robot_path);
+    double total_distance = cleaning_robot.calculatePath(distance);
     std::cout << "The total_distance is: " << total_distance << std::endl;
 
     //Calculate the cleaning Area 
-    //ouble cleaning_area = cleaning_robot.calculateCleaningArea(robot, robot_cleaning_gadget, robot_path);
+    //double cleaning_area = cleaning_robot.calculateCleaningArea(robot, robot_cleaning_gadget, robot_path);
     //std::cout << "CleaningArea: " << cleaning_area << std::endl;
+
+    // Calculate the time for the complete path depends on the velocity from the robot
+    double total_time = cleaning_robot.calculateTimeOfPath(distance);
+    std::cout << "The total travel time is: " << total_time << std::endl;
 
     ros::spin();
 }
